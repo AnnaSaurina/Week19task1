78b08d10073c086c9d97bf252e803c0a605a2fe2
AnnaSaurina <the_happy_ann@mail.ru>
2023-07-26 22:28:27 +0300

    add style css

--- assets/index.js
+++ assets/index.js
@@ -2,23 +2,23 @@ const wrapper =  document.querySelector('.wrapper');
 
 function getPost () {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(responce => responce.json())
-    .then(json => {
-        json.forEach((element) => {
-            console.log(element.title);
-            console.log(element.body);
-            const box = document.querySelector('div');
-            box.classList.add('box_post');
-            box.innerHTML = `
-            <div class="title">${element.title}</div>
-            <div class="text">${element.body}</div>
-            `;
-            wrapper.appendChild(box);
-        });
-    })
-    .catch((err) => {
-        console.log('Error' + err)
-    })
+        .then(response => response.json())
+        .then(json => {
+            json.forEach((element) => {
+                console.log(element.title);
+                console.log(element.body);
+                const box = document.createElement('div');
+                box.classList.add('box_post');
+                box.innerHTML = `
+                <div class="title">Title${element.title}</div>
+                <div class="text">Text${element.body}</div>
+                `;
+                wrapper.appendChild(box);
+            });
+        })
+        .catch((err) => {
+            console.log('Error' + err)
+        })
 
 }
 
